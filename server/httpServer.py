64b5495d000a4d2ea34f4dfc9422017f14449963
Psychpsyo <psychpsyo@gmail.com>
2022-11-24 02:59:54 +0100

    Actually use /api endpoint

--- server/httpServer.py
+++ server/httpServer.py
@@ -5,71 +5,71 @@ import json
 
 class HttpServer(BaseHTTPRequestHandler):
 	def do_POST(self):
-		#try:
-		query = json.loads(self.rfile.read(int(self.headers.get('Content-Length'))))
-		if query["action"] == "createAccount":
-			userInfo = db.createUser(query["name"], query["password"], query["inviteCode"])
-			self.respondOK({"userID": userInfo[0], "token": userInfo[1]})
-			return;
-		
-		if query["action"] == "login":
-			userInfo = db.getTokenAndID(query["name"], query["password"])
-			self.respondOK({"userID": userInfo[0], "token": userInfo[1]})
-			return;
-		
-		if query["action"] == "logout":
-			db.invalidateToken(query["userID"], query["token"])
-			self.respondOK(None)
-			return;
-		
-		if query["action"] == "verifyToken":
-			isValid = db.authenticateToken(query["userID"], query["token"])
-			self.respondOK({"verified": isValid})
-			return;
-		
-		if query["action"] == "createThread":
-			threadID = db.createThread(query["userID"], query["token"], query["title"], query["content"])
-			self.respondOK({"threadID": threadID})
-			return;
-		
-		if query["action"] == "createPost":
-			postID = db.createPost(query["userID"], query["token"], query["threadID"], query["content"])
-			self.respondOK({"postID": postID})
-			return;
-		
-		if query["action"] == "getThreads":
-			threads = db.getThreads(query["userID"], query["token"], query["page"], 10)
-			threadCount = db.getThreadCount(query["userID"], query["token"])
-			self.respondOK({"threads": threads, "threadCount": threadCount})
-			return;
-		
-		if query["action"] == "getPosts":
-			posts = db.getPosts(query["userID"], query["token"], query["threadID"], query["page"], 15)
-			self.respondOK({"posts": posts})
-			return;
-		
-		if query["action"] == "getUserInfo":
-			user = db.getUserInfo(query["userID"], query["token"], query["requestedUserID"])
-			self.respondOK({"user": user})
-			return;
-		
-		if query["action"] == "getThreadInfo":
-			thread = db.getThreadInfo(query["userID"], query["token"], query["threadID"])
-			self.respondOK({"thread": thread})
-			return;
-		
-		self.send_response(400)
-		self.send_header("Access-Control-Allow-Origin", "*")
-		self.send_header("Content-type", "application/json")
-		self.end_headers()
-		self.wfile.write(bytes(json.dumps({"error": "unknownAction"}), "utf-8"))
-		#except Exception as e:
-		#	print(e)
-		#	self.send_response(500)
-		#	self.send_header("Access-Control-Allow-Origin", "*")
-		#	self.send_header("Content-type", "application/json")
-		#	self.end_headers()
-		#	self.wfile.write(bytes(json.dumps({"error": "unknown error"}), "utf-8"))
+		try:
+			query = json.loads(self.rfile.read(int(self.headers.get('Content-Length'))))
+			if query["action"] == "createAccount":
+				userInfo = db.createUser(query["name"], query["password"], query["inviteCode"])
+				self.respondOK({"userID": userInfo[0], "token": userInfo[1]})
+				return;
+			
+			if query["action"] == "login":
+				userInfo = db.getTokenAndID(query["name"], query["password"])
+				self.respondOK({"userID": userInfo[0], "token": userInfo[1]})
+				return;
+			
+			if query["action"] == "logout":
+				db.invalidateToken(query["userID"], query["token"])
+				self.respondOK(None)
+				return;
+			
+			if query["action"] == "verifyToken":
+				isValid = db.authenticateToken(query["userID"], query["token"])
+				self.respondOK({"verified": isValid})
+				return;
+			
+			if query["action"] == "createThread":
+				threadID = db.createThread(query["userID"], query["token"], query["title"], query["content"])
+				self.respondOK({"threadID": threadID})
+				return;
+			
+			if query["action"] == "createPost":
+				postID = db.createPost(query["userID"], query["token"], query["threadID"], query["content"])
+				self.respondOK({"postID": postID})
+				return;
+			
+			if query["action"] == "getThreads":
+				threads = db.getThreads(query["userID"], query["token"], query["page"], 10)
+				threadCount = db.getThreadCount(query["userID"], query["token"])
+				self.respondOK({"threads": threads, "threadCount": threadCount})
+				return;
+			
+			if query["action"] == "getPosts":
+				posts = db.getPosts(query["userID"], query["token"], query["threadID"], query["page"], 15)
+				self.respondOK({"posts": posts})
+				return;
+			
+			if query["action"] == "getUserInfo":
+				user = db.getUserInfo(query["userID"], query["token"], query["requestedUserID"])
+				self.respondOK({"user": user})
+				return;
+			
+			if query["action"] == "getThreadInfo":
+				thread = db.getThreadInfo(query["userID"], query["token"], query["threadID"])
+				self.respondOK({"thread": thread})
+				return;
+			
+			self.send_response(400)
+			self.send_header("Access-Control-Allow-Origin", "*")
+			self.send_header("Content-type", "application/json")
+			self.end_headers()
+			self.wfile.write(bytes(json.dumps({"error": "unknownAction"}), "utf-8"))
+		except Exception as e:
+			print(e)
+			self.send_response(500)
+			self.send_header("Access-Control-Allow-Origin", "*")
+			self.send_header("Content-type", "application/json")
+			self.end_headers()
+			self.wfile.write(bytes(json.dumps({"error": "unknown error"}), "utf-8"))
 	
 	def respondOK(self, jsonData):
 		print(jsonData)
