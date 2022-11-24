6ea34c035c03254f4e62a8cdae2382053fc68de4
Psychpsyo <psychpsyo@gmail.com>
2022-11-25 00:36:50 +0100

    bugfixes
    background no longer pixelated
    <title> capitalization
    settings update themselves on pageload

--- website/settings.js
+++ website/settings.js
@@ -1,6 +1,8 @@
 // load initial settings
 setTheme(localStorage.getItem("theme")? localStorage.getItem("theme") : "light");
+settingsTheme.value = localStorage.getItem("theme");
 localStorage.setItem("postsPerPage", localStorage.getItem("postsPerPage")? localStorage.getItem("postsPerPage") : "15");
+settingsPostsPerPage.value = localStorage.getItem("postsPerPage");
 
 // settings menu
 settingsButton.addEventListener("click", function() {
